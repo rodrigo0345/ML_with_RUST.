383a88874e7bb3a44a1774a1474023af6656b180
Rodrigo Casanova <rodrigocralha@gmail.com>
2023-06-10 17:17:11 +0100

    adding a bias to the whole system, made the cost approximate to 0 a lot more!

--- src/gates.rs
+++ src/gates.rs
@@ -1,5 +1,3 @@
-use std::intrinsics::expf64;
-
 use rand::Rng;
 
 #[derive(Debug, Clone)]
@@ -28,10 +26,10 @@ impl TrainingData {
 }
 
 fn sigmoid_float(x: f64) -> f64 {
-    return 1.0 / (1.0 - (-x).exp()) as f64;
+    return 1.0 / (1.0 + (-x).exp()) as f64;
 }
 
-fn cost(w: Vec<f64>, training_data: &Vec<Vec<f64>>) -> f64 {
+fn cost(w: Vec<f64>, b: f64, training_data: &Vec<Vec<f64>>) -> f64 {
     let mut result: f64 = 0.0;
     let n_parameters = w.len();
 
@@ -45,11 +43,12 @@ fn cost(w: Vec<f64>, training_data: &Vec<Vec<f64>>) -> f64 {
             y += x * w;
         }
         let expected = point[n_parameters];
-        let d = y - expected;
+        let sig_y = sigmoid_float(y + b);
+        let d = sig_y - expected;
         result += d * d;
     });
 
-    result /= w.len() as f64;
+    result /= (training_data.len()) as f64;
     return result;
 }
 
@@ -59,27 +58,41 @@ pub fn gates() {
 
     let mut w1 = rand.gen::<f64>();
     let mut w2 = rand.gen::<f64>();
+    let mut b = rand.gen::<f64>();
 
-    let eps = 1e-3;
-    let rate = 1e-3;
+    let eps = 1e-1;
+    let rate = 1e-1;
 
     let data = &training_data.to_owned().or_data;
 
-    for _ in 0..4000 {
-        let c = cost(Vec::from([w1, w2]), data);
+    for _ in 0..1000 * 1000 {
+        let c = cost(Vec::from([w1, w2]), b, data);
 
-        println!("w1: {}, w2: {}, cost: {}", w1, w2, c);
+        let dw1 = (cost(Vec::from([w1 + eps, w2]), b, data) - c) / eps;
 
-        let dw1 = (cost(Vec::from([w1 + eps, w2]), data) - c) / eps;
+        let dw2 = (cost(Vec::from([w1, w2 + eps]), b, data) - c) / eps;
 
-        let dw2 = (cost(Vec::from([w1, w2 + eps]), data) - c) / eps;
+        let db = (cost(Vec::from([w1, w2]), b + eps, data) - c) / eps;
 
         w1 -= rate * dw1;
         w2 -= rate * dw2;
+        b -= rate * db;
     }
 
-    println!("x1 = 0 | x2 = 1 | r = {}", w1 * 0.0 + w2 * 1.0);
-    println!("x1 = 1 | x2 = 1 | r = {}", w1 * 1.0 + w2 * 1.0);
-    println!("x1 = 0 | x2 = 0 | r = {}", w1 * 0.0 + w2 * 0.0);
-    println!("x1 = 1 | x2 = 0 | r = {}", w1 * 1.0 + w2 * 0.0);
+    println!(
+        "w1: {}, w2: {}, cost: {}",
+        w1,
+        w2,
+        cost(Vec::from([w1, w2]), b, data)
+    );
+
+    data.into_iter().for_each(|point| {
+        println!(
+            "{} | {} = {}, expected: {}",
+            point[0],
+            point[1],
+            sigmoid_float(point[0] * w1 + point[1] * w2),
+            point[2]
+        );
+    });
 }
