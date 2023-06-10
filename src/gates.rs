ab9f7f1427003889d89a79e2b72db23dc09b174a
Rodrigo Casanova <rodrigocralha@gmail.com>
2023-06-10 19:52:30 +0100

    gates, now I need to go to the Xor...

--- src/gates.rs
+++ src/gates.rs
@@ -5,7 +5,6 @@ struct TrainingData {
     or_data: Vec<Vec<f64>>,
     and_data: Vec<Vec<f64>>,
     nand_data: Vec<Vec<f64>>,
-    xor_data: Vec<Vec<f64>>,
 }
 
 impl TrainingData {
@@ -21,7 +20,6 @@ impl TrainingData {
             and_data: Vec::from([
                 Vec::from([0.0, 0.0, 0.0]),
                 Vec::from([0.3, 0.0, 0.0]),
-                Vec::from([5.0, 0.0, 0.0]),
                 Vec::from([1.0, 0.0, 0.0]),
                 Vec::from([0.0, 1.0, 0.0]),
                 Vec::from([1.0, 1.0, 1.0]),
@@ -32,69 +30,7 @@ impl TrainingData {
                 Vec::from([0.0, 1.0, 1.0]),
                 Vec::from([1.0, 1.0, 0.0]),
             ]),
-            xor_data: Vec::from([
-                Vec::from([0.0, 0.0, 0.0]),
-                Vec::from([1.0, 0.0, 1.0]),
-                Vec::from([0.0, 1.0, 1.0]),
-                Vec::from([1.0, 1.0, 0.0]),
-            ]),
         }
-    }
-}
-
-struct Neuron {
-    pub weights: Vec<f64>,
-    pub b: f64,
-}
-
-impl Neuron {
-    fn new(weights: Vec<f64>, b: f64) -> Neuron {
-        Neuron {
-            weights: weights,
-            b: b,
-        }
-    }
-
-    fn new_default() -> Neuron {
-        let mut rand = rand::thread_rng();
-        let weights = Vec::from([rand.gen::<f64>(), rand.gen::<f64>()]);
-
-        Neuron {
-            weights: weights,
-            b: rand.gen::<f64>(),
-        }
-    }
-
-    fn one_iter(&mut self, data: &Vec<Vec<f64>>, eps: f64, rate: f64) {
-        let c = cost(self.weights.to_owned(), self.b, data);
-
-        let dw1 = (cost(
-            Vec::from([self.weights[0] + eps, self.weights[1]]),
-            self.b,
-            data,
-        ) - c)
-            / eps;
-
-        let dw2 = (cost(
-            Vec::from([self.weights[0], self.weights[1] + eps]),
-            self.b,
-            data,
-        ) - c)
-            / eps;
-
-        let db = (cost(self.weights.to_owned(), self.b + eps, data) - c) / eps;
-
-        let w1 = self.weights[0] - rate * dw1;
-        let w2 = self.weights[1] - rate * dw2;
-
-        self.weights.clear();
-        self.weights.push(w1);
-        self.weights.push(w2);
-        self.b -= rate * db;
-    }
-
-    fn result(&self, x: f64, y: f64) -> f64 {
-        return sigmoid_float(x * self.weights[0] + y * self.weights[1] + self.b);
     }
 }
 
@@ -109,7 +45,7 @@ fn cost(w: Vec<f64>, b: f64, training_data: &Vec<Vec<f64>>) -> f64 {
     training_data.into_iter().for_each(|point| {
         let mut y = 0.0;
 
-        // y = x1 * w1 + x2 * w2 + b
+        // y = x1 * w1 + x2 * w2
         for i in 0..n_parameters {
             let x = point[i];
             let w = w[i];
@@ -126,45 +62,38 @@ fn cost(w: Vec<f64>, b: f64, training_data: &Vec<Vec<f64>>) -> f64 {
 }
 
 pub fn gates() {
+    let mut rand = rand::thread_rng();
     let training_data = TrainingData::new();
 
-    let mut neuron_or = Neuron::new_default();
-    let mut neuron_nand = Neuron::new_default();
-    let mut neuron_and = Neuron::new_default();
+    let mut w1 = rand.gen::<f64>();
+    let mut w2 = rand.gen::<f64>();
+    let mut b = rand.gen::<f64>();
 
     let eps = 1e-1;
     let rate = 1e-1;
 
-    let data = &training_data.to_owned().xor_data;
+    let data = &training_data.to_owned().or_data;
 
-    for _ in 0..10000 {
-        let x = neuron_and.weights[0];
-        let y = neuron_and.weights[1];
+    for _ in 0..1000 * 1000 {
+        let c = cost(Vec::from([w1, w2]), b, data);
 
-        neuron_or.one_iter(data, eps, rate);
-        let a = neuron_or.result(x, y);
+        let dw1 = (cost(Vec::from([w1 + eps, w2]), b, data) - c) / eps;
 
-        neuron_nand.one_iter(data, eps, rate);
-        let b = neuron_nand.result(x, y);
+        let dw2 = (cost(Vec::from([w1, w2 + eps]), b, data) - c) / eps;
 
-        neuron_and.weights = Vec::from([a, b]);
-        neuron_and.one_iter(data, eps, rate);
+        let db = (cost(Vec::from([w1, w2]), b + eps, data) - c) / eps;
 
-        println!(
-            "w1: {}, w2: {}, b: {}, cost: {}",
-            neuron_and.weights[0],
-            neuron_and.weights[1],
-            neuron_and.b,
-            cost(neuron_and.weights.to_owned(), neuron_and.b, data)
-        );
+        w1 -= rate * dw1;
+        w2 -= rate * dw2;
+        b -= rate * db;
     }
 
     println!(
         "w1: {}, w2: {}, b: {}, cost: {}",
-        neuron_and.weights[0],
-        neuron_and.weights[1],
-        neuron_and.b,
-        cost(neuron_and.weights.to_owned(), neuron_and.b, data)
+        w1,
+        w2,
+        b,
+        cost(Vec::from([w1, w2]), b, data)
     );
 
     data.into_iter().for_each(|point| {
@@ -172,10 +101,10 @@ pub fn gates() {
             "{} | {} = {}, expected: {}",
             point[0],
             point[1],
-            sigmoid_float(
-                point[0] * neuron_and.weights[0] + point[1] * neuron_and.weights[1] + neuron_and.b
-            ),
+            sigmoid_float(point[0] * w1 + point[1] * w2 + b),
             point[2]
         );
     });
+
+    println!("3 | 0 = {}", sigmoid_float(3.0 * w1 + 0.0 * w2 + b));
 }
